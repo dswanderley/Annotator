fb7896f286686c39ac12c6d372c573d36f80f683
ARGONATH\Diego <dswanderley@gmail.com>
2018-08-28 12:01:41 +0100

    ADD: Start annotator web project with node.js

---
+++ app.js
@@ -0,0 +1,47 @@
+/*
+ * Module dependencies
+ */
+var express = require('express'),
+    logger = require('morgan'),
+    stylus = require('stylus'),
+    nib = require('nib');
+
+// Initialize express instance
+var app = express();
+// Init stylus
+function compile(str, path) {
+	return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
+// Define Views path and engines
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(logger('dev'))
+
+// Define public directory
+app.use(stylus.middleware({ 
+    src: __dirname + '/public',
+    compile: compile
+}))
+app.use(express.static(__dirname + '/public'))
+// Image uploaded directory
+app.use(stylus.middleware({
+    src: __dirname + '/images',
+    compile: compile
+}))
+app.use(express.static(__dirname + '/images'))
+
+// Create External Routes
+
+// Index
+var index = require('./routes/index');
+app.use(index);
+// Annotations 
+var pilot = require('./routes/annot');
+app.use(pilot);
+// Gallery
+app.use('/gallery', pilot);
+
+// Deploy
+app.listen(3000)
---
+++ routes/annot.js
@@ -0,0 +1,52 @@
+/*
+*   pilot.js
+*   Router responsible to manage pilot application calls
+*/
+
+// Module dependencies
+var router = require('express').Router(),
+    fs = require('fs'),
+    sizeOf = require('image-size');
+
+var galleryDir = './images/gallery/';
+
+// Annotations - GET
+router.get('/annot', function (req, res) {
+    // Render page Pilot
+    res.render('./annot', {
+        title: 'Annotations'
+    })
+});
+
+// Gallery - GET
+router.get('/gallery', function (req, res) {
+    // Initialize list of files
+    file_list = [];
+    gallery_list = [];
+    // Read json file
+    var obj = JSON.parse(fs.readFileSync(galleryDir + 'gallery.json', 'utf8'));
+    // Read directory
+    fs.readdir(galleryDir, (err, files) => {
+        // Load files
+        files.forEach(file => {
+            
+            obj.galleryitem.forEach(function (el) {
+
+                if (el.filename === file) {
+                    file_list.push(file); // add to file list
+                    // Get Dimensions
+                    var dimensions = sizeOf(galleryDir + file);
+                    el.width = dimensions.width;
+                    el.height = dimensions.height;
+                    // add file informations 
+                    gallery_list.push(el);
+                }
+            });            
+        });
+        // Send list of files
+        res.send({file_list, gallery_list});
+    });    
+});
+
+// Return routers
+module.exports = router;
