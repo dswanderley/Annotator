c184f0c343645c774c4af4986938e223386ee6ab
ARGONATH\Diego <dswanderley@gmail.com>
2018-08-28 15:06:42 +0100

    CHANGE: Clear context (jade, JS and css), main row fit the page.

--- public/javascripts/annot.js
+++ public/javascripts/annot.js
@@ -30,44 +30,21 @@ function loadScreenDrApp() {
     /** @description Initialize componentes of the application
      */
     $('#res-field-map').css('visibility', 'hidden');
-    setEvalBtn();
     setScreenSize();
     addEvents();
-    loadGallery();
-    initExamples();
-}
-
-function initExamples() {
-    /** @description Set example buttons and images
-     */
-    clearBtnQualEg();
-    clearBtnDrEg();
-    setEgImg('/images/quality_high.png');
-    $('#btn-qual-high').addClass('focus');
-    $('#btn-dr-r0').addClass('focus');
+    //loadGallery();
 }
 
 function setScreenSize() {
     /** @description Defines max image heigh according other page elements
      *  Size of all other elements are predefined.
      */
-    height_header = 76;
-    height_footer = 115;
-    height_window = $(window).height();
 
-    height_body = height_window - height_header - height_footer;
-
-    height_gallery = 86 + 15 + 20;
-    height_breadcrumb = 19 + 12 + 12 + 16;
-    padding_image = 5 + 5;
-
-    max_img_height = height_body - height_gallery - height_breadcrumb - padding_image - 3;
-    if (max_img_height < 256)
-        max_img_height = 256;
-
-    max_img_width = $('#gallery').width();
+    max_height = $(window).height() - $('#footer').height() - $('#header').height();;
+    $('#app-row').height(max_height);
 
     setCanvasSize();
+    
 }
 
 function refreshScreenSize() {
@@ -168,139 +145,6 @@ function loadGallery() {
         });
 }
 
-function quality() {
-    /** @description Call image Quality. Evaluate displayed image.
-     */
-    // Read image filename
-    $('.loader').show()
-    var currentSrc = img_orig;
-    var str_list = currentSrc.split('/');
-    img = str_list[str_list.length - 1];
-    folder = str_list[str_list.length - 2];
-    // Ajax call
-    $.ajax(
-        {
-            type: 'GET',
-            url: urlBase + '/quality',
-            data: {
-                dir: folder,
-                img: img
-            },
-            dataType: 'json',
-            cache: false,
-            async: true,
-            success: function (data) {
-                // Convert data to JSON
-                qual_data = data;
-                // Print results
-                $('#res-field-qual').css('visibility', 'visible');
-                //$('#lbl-res1').text('Percentage: ' + Math.round(qual_data.q_pred) + '% ');
-                $('#lbl-res2').text(qual_data.qual);
-
-                console.log("Quality: " + qual_data.qual);
-                console.log("Prediction Val.: " + qual_data.q_pred);
-                // Get image path and URL
-                path = qual_data.path;
-                if (qual_data.q_pred <= 50) {
-                    // Quality flag
-                    hasQuality = false;
-                    // Partial cases
-                    if (qual_data.q_pred > 25) {
-                        hasQuality = true;
-                    }
-                    //  Image src
-                    img_qual = path;
-                    currentSrc = img_qual;
-                    // Pre Load image
-                    var img = new Image();
-                    img.onload = function () {
-                        setMainImage();
-                        $('.loader').hide();
-                        toogleBtnClick();
-                    }
-                    img.src = currentSrc;
-                    // Set css attributes
-                    $('#img-disp').attr('height', '256px');
-                    $('#lbl-res2').addClass("btn-outline-danger");
-                    $('#res-field-map').css('visibility', 'visible');
-                    $('.onoffswitch2-checkbox').prop('checked', true);
-                    $('#res-field-map').css('visibility', 'visible');
-                    $('#lbl-res5').text('Quality Map');
-                }
-                else {
-                    hasQuality = true;
-                    $('#lbl-res2').addClass("btn-outline-success");
-                    $('#res-field-map').css('visibility', 'hidden');
-                }
-                // Block or allow Btn
-                setEvalBtn();
-                $('.loader').hide();
-            }
-        });
-}
-
-function dr_detection() {
-    /** @description Call DR detection. Process displayed image.
-     */
-    $('.loader').show()
-    // Read image filename
-    var currentSrc = img_orig;
-    var str_list = currentSrc.split('/');
-    img = str_list[str_list.length - 1];
-    folder = str_list[str_list.length - 2];
-    // Ajax call
-    $.ajax(
-        {
-            type: 'GET',
-            url: urlBase + '/dr_detection',
-            data: {
-                dir: folder,
-                img: img
-            },
-            dataType: 'json',
-            cache: false,
-            async: true,
-            success: function (data) {
-                // Convert data to JSON
-                dr_data = data;
-                // Print results
-                $('#res-field-dr').css('visibility', 'visible');
-                //$('#lbl-res3').text('Percentage: ' + Math.round(dr_data.dr_pred) + '% ');
-                $('#lbl-res4').text(dr_data.dr);
-
-                console.log("Disease: " + dr_data.dr);
-                console.log("Prediction Val.: " + dr_data.dr_pred);
-                // Get image path and URL
-                path = dr_data.path;
-                if (dr_data.dr_pred > 50) {
-                    // Image src
-                    img_dr = path;
-                    currentSrc = img_dr;
-                    // Pre Load image
-                    var img = new Image();
-                    img.onload = function () {
-                        setMainImage();
-                        $('.loader').hide();
-                        toogleBtnClick();
-                    }
-                    img.src = currentSrc;
-                    // Set css attributes
-                    $('#img-disp').attr('height', '256px');
-                    $('#lbl-res4').addClass("btn-outline-danger");
-                    $('#res-field-map').css('visibility', 'visible');
-                    $('.onoffswitch2-checkbox').prop('checked', true);
-                    $('#lbl-res5').text('Detection Map');
-                }
-                else {
-                    $('#lbl-res4').addClass("btn-outline-success");
-                    $('#res-field-map').css('visibility', 'hidden');
-                }
-                $('.loader').hide();
-
-            }
-        });
-}
-
 
 /*
  * Set Image
@@ -379,8 +223,8 @@ function setCanvasSize() {
     canvas.width = canvas_width;
     canvas.height = canvas_height;
     // CSS content 
-    canvas.style.width = canvas_width.toString() + "px";
-    canvas.style.height = canvas_height.toString() + "px";
+    canvas.style.width = canvas.width.toString() + "px";
+    canvas.style.height = canvas.height.toString() + "px";
     // Canvas context
     ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
@@ -562,234 +406,6 @@ function redraw(reset) {
     ctx.drawImage(main_img, canvas_cx, canvas_cy, main_img.width, main_img.height, canvas_dx, canvas_dy, img_dwidth, img_dheight);
 }
 
-
-/*
- * Set Results
- */
-
-function resetimages() {
-    /** @description Reset image reference src
-     */
-    img_qual = "";
-    img_dr = "";
-    $('#res-field-map').css('visibility', 'hidden');
-}
-
-function setEvalBtn() {
-    /** @description Enable or disable evaluation buttons according quality
-     */
-    if (hasQuality) {
-        $('#btn-dr').removeAttr("disabled").button('refresh');
-    }
-    else {
-        $('#btn-dr').attr("disabled", "disabled").button('refresh');
-    }
-}
-
-function resetLbl() {
-    /** @description Reset labels 
-     */
-    $('#lbl-res2').text('');
-    $('#lbl-res4').text('');
-    $('#res-field-qual').css('visibility', 'hidden');
-    $('#res-field-dr').css('visibility', 'hidden');
-    $('#res-field-map').css('visibility', 'hidden');
-    $('#lbl-res2').removeClass("btn-outline-danger");
-    $('#lbl-res2').removeClass("btn-outline-success");
-    $('#lbl-res4').removeClass("btn-outline-danger");
-    $('#lbl-res4').removeClass("btn-outline-success");
-}
-
-function toogleBtnClick() {
-    /** @description Set main image according button status
-     */
-    if ($('.onoffswitch2-checkbox').is(":checked")) {
-
-        if (img_dr == "") {
-            if (img_qual != "") {
-                currentSrc = img_qual;
-                setMainImage();
-            }
-        }
-        else {
-            currentSrc = img_dr;
-            setMainImage();
-        }
-    }
-    else {
-        currentSrc = img_orig;
-        setMainImage();
-    }
-}
-
-/*
- * Set Examples
- */
-
-function setImgEg(id) {
-    /** @description Initiate example image with a R0 image
-     * @param {int} id Image gallery id
-     */
-    // Set example
-    imgInfo = galleryData[id];
-    // URL
-    setEgImg(galleryURL + imgInfo.filename);
-    // Grading
-    changeDrEg(imgInfo.grading);
-    // Quality
-    changeQualEg(imgInfo.quality);
-}
-
-function setImgQualEg(click_id) {
-    /** @description Set image of quality image example
-     * @param {string} image src
-     */
-    if (click_id == 'btn-qual-high') {
-        qual = 'High';
-    }
-    else {
-        qual = 'Low';
-    }
-    // Pick an index
-    idx = Math.floor(Math.random() * galleryData.length);
-    // Create an auxiliary list starting by the sorted index
-    auxlist1 = galleryData.slice(idx, galleryData.length);
-    auxlist2 = galleryData.slice(0, idx);
-    auxlist = auxlist1.concat(auxlist2);
-    // Find the next image in the list
-    for (i = 0; i < auxlist.length; i++) {
-        el = auxlist[i];
-        if (el.quality === qual) {
-            src = galleryURL + el.filename;
-            // Set example image
-            setEgImg(src);
-            changeDrEg(el.grading);
-            break;
-        }
-    }
-}
-
-function setImgDrEg(click_id) {
-    /** @description Set image of DR image example
-     * @param {string} image src
-     */
-
-    // Verify the selected gradding
-    switch (click_id) {
-        case 'btn-dr-r0':
-            grad = 'R0';
-            break;
-        case 'btn-dr-r1':
-            grad = 'R1';
-            break;
-        case 'btn-dr-r2':
-            grad = 'R2';
-            break;
-        case 'btn-dr-r3':
-            grad = 'R3';
-            break;
-        default:
-            grad = 'RX';
-    }
-    // Pick an index
-    idx = Math.floor(Math.random() * galleryData.length);
-    // Create an auxiliary list starting by the sorted index
-    auxlist1 = galleryData.slice(idx, galleryData.length);
-    auxlist2 = galleryData.slice(0, idx);
-    auxlist = auxlist1.concat(auxlist2);
-    // Find the next image in the list
-    for (i = 0; i < auxlist.length; i++) {
-        el = auxlist[i];
-        if (el.grading === grad) {
-            src = galleryURL + el.filename;
-            // Set example image
-            setEgImg(src);
-            changeQualEg(el.quality);
-            break;
-        }
-    }
-
-}
-
-function setEgImg(src) {
-    /** @description Set image of image example
-    * @param {string} image src
-    */
-    $('#img-eg-dr')[0].src = src;
-}
-
-function setQualEg(btn) {
-    /** @description Manage the Quality example buttons 
-    * @param {obj} Button
-     */
-    setImgQualEg(btn.id);
-    clearBtnQualEg();
-    $('#' + btn.id).addClass('focus');
-}
-
-function setDrEg(btn) {
-    /** @description Manage the DR example buttons 
-    * @param {obj} Button
-    */
-    setImgDrEg(btn.id);
-    clearBtnDrEg();
-    $('#' + btn.id).addClass('focus');
-}
-
-function changeQualEg(qual) {
-    /** @description Change quality example button 
-    * @param {string} quality
-     */
-    switch (qual) {
-        case 'High':
-            btn_q_id = '#btn-qual-high';
-            break;
-        default:
-            btn_q_id = '#btn-qual-low';
-    }
-    clearBtnQualEg();
-    $(btn_q_id).addClass('focus');
-}
-
-function changeDrEg(grad) {
-    /** @description Change quality example button 
-    * @param {string} grading
-     */
-    switch (grad) {
-        case 'R0':
-            btn_rd_id = '#btn-dr-r0';
-        case 'R1':
-            btn_rd_id = '#btn-dr-r1';
-            break;
-        case 'R2':
-            btn_rd_id = '#btn-dr-r2';
-            break;
-        case 'R3':
-            btn_rd_id = '#btn-dr-r3';;
-            break;
-        default:
-            btn_rd_id = '#btn-dr-rx';
-    }
-    clearBtnDrEg();
-    $(btn_rd_id).addClass('focus');
-}
-
-function clearBtnQualEg() {
-    /** @description Remove classes from Quality Examples Buttons
-    */
-    $('#btn-qual-high').removeClass('focus');
-    $('#btn-qual-low').removeClass('focus');
-}
-
-function clearBtnDrEg() {
-    /** @description Remove classes from DR Examples Buttons
-    */
-    $('#btn-dr-r0').removeClass('focus');
-    $('#btn-dr-r1').removeClass('focus');
-    $('#btn-dr-r2').removeClass('focus');
-    $('#btn-dr-r3').removeClass('focus');
-    $('#btn-dr-rx').removeClass('focus');
-}
 
 
 /*
