ba618b254888b921b626bba0eadc1cbe002f7592
ARGONATH\Diego <dswanderley@gmail.com>
2018-08-29 14:34:40 +0100

    CHANGE: Init canvas ADD: Canvas controls

--- public/javascripts/annot.js
+++ public/javascripts/annot.js
@@ -22,25 +22,21 @@ function loadScreenDrApp() {
      */
     $('#res-field-map').css('visibility', 'hidden');
     setScreenSize();
-    addEvents();
-
     currentSrc = 'screen/20170317_0001.png'
-    setMainImage();
-    
-    //loadGallery();
+    initCanvas(currentSrc);
+    addEvents();
 }
 
 function setScreenSize() {
     /** @description Defines max image heigh according other page elements
      *  Size of all other elements are predefined.
      */
-
+    
     max_img_height = $(window).height() - $('#footer').height() - $('#header').height();
     max_img_width = $('#col-diag-center').width();
-    
+    // Set background of canvas 
     $('#app-row').height(max_img_height);
 
-    setCanvasSize();    
 }
 
 function refreshScreenSize() {
@@ -53,7 +49,7 @@ function refreshScreenSize() {
 
 function addEvents() {
     /** @description Add Events listener
-     */
+     */    
     canvas.addEventListener('mousedown', canvasMouseDown, false);
     canvas.addEventListener('mousemove', canvasMouseMove, false);
     canvas.addEventListener('mouseup', canvasMouseUp, false);
@@ -61,8 +57,8 @@ function addEvents() {
     canvas.addEventListener("mousewheel", canvasScrollWheel, false);
     // Firefox
     canvas.addEventListener("DOMMouseScroll", canvasScrollWheel, false);
-
     document.addEventListener('mouseup', pageMouseUp, false);
+
 }
 
 function pageMouseUp(evt) {
@@ -77,90 +73,185 @@ function pageMouseUp(evt) {
 
 
 /*
- * Set Image
- */
-
-
-function setMainImage() {
-    /** @description Set original image src
-     */
-    redraw(true);
-    w = 864;//galleryData[current_idx].width;
-    h = 768;//galleryData[current_idx].height;
-    // Set Canvas parameters
-    setCanvasParameters(w, h);
-    // Update image
-    refreshCanvasImg();
-    refreshCanvasImg();
-}
-
-/*
  * Canvas
  */
 
-function setCanvasSize() {
-    /** @description Set canvas size 
+
+function initCanvas(src) {
+    /** @description Initialize canvas
+      * @param {sting} src 
      */
-    // Create main canvas 
+
+    // Load image on canvas
+    img = new Image();
+    var img_width = 864;
+    var img_height = 768;
+
+    // Canvas size
     canvas_width = max_img_width;
     canvas_height = max_img_height;
+    // load canvas
     canvas = document.getElementById("main-canvas");
-    canvas.width = canvas_width;
-    canvas.height = canvas_height;
+    // set canvas dimensions
+    canvas.width = Math.floor(canvas_width);
+    canvas.height = Math.floor(canvas_height - 30);
     // CSS content 
     canvas.style.width = canvas.width.toString() + "px";
     canvas.style.height = canvas.height.toString() + "px";
-    // Canvas context
-    ctx = canvas.getContext('2d');
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    // Load context
+    ctx = canvas.getContext("2d");
     trackTransforms(ctx);
-}
-
-function setCanvasParameters(w, h) {
-    /** @description set canvas dimensions parameters
-      * @param {int} w width
-      * @param {int} h height
-     */
-
-    // Original image size
-    img_width = w;
-    img_height = h;
-    // Display image size
-    img_dwidth = canvasScale * img_width;
-    img_dheight = canvasScale * img_height;
     // Calculate scale with canvas size
-    canvasScale = Math.min(canvas.height / img_height, canvas.width / img_width);
-    // coordinate in the destination canvas at which to place the top-left corner of the source image
-    canvas_dx = (canvas.width - img_width * canvasScale) / 2;
-    canvas_dy = (canvas.height - img_height * canvasScale) / 2;
-    //coordinate of the top left corner of the sub-rectangle of the source image to draw into the destination context
-    canvas_cx = 0;
-    canvas_cy = 0;
+    this.canvasScale = Math.min(canvas.height / img_height, canvas.width / img_width);
+    // Calculate start postions
+    x = Math.round((canvas.width - img_width)/2);
+    y = Math.round((canvas.height - img_height)/2);
+    // Image on canvas dimensions
+    csizes = new CanvasSizes(x, y, img_width, img_height, 0, 0, img_width, img_height);
+    ctx.drawImage(img, csizes.cropX, csizes.cropY, csizes.cropW, csizes.cropH, csizes.canvasX, csizes.canvasY, csizes.canvasW, csizes.canvasH);
+    img.onload = function () {
+        ctx.drawImage(img, csizes.cropX, csizes.cropY, csizes.cropW, csizes.cropH, csizes.canvasX, csizes.canvasY, csizes.canvasW, csizes.canvasH);
+    }
+
+    img.src = src;
 }
 
-function refreshCanvasImg() {
-    /** @description Refresh canvas image
+function refreshCanvas() {
+    /** @description Refresh canvas routine
+    */
+
+    // Get new coord
+    var p1 = ctx.transformedPoint(0, 0);
+    var p2 = ctx.transformedPoint(canvas.width, canvas.height);
+    // Crop canvas
+    this.ctx.clearRect(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y);
+    // Draw image
+    this.ctx.drawImage(img, csizes.cropX, csizes.cropY, csizes.cropW, csizes.cropH, csizes.canvasX, csizes.canvasY, csizes.canvasW, csizes.canvasH);
+    
+}
+
+function CanvasSizes(x, y, w, h, cX, cY, cW, cH) {
+    /** @description Classe with canvas dimensions
+      * @param {int} x The x coordinate where to place the image on the canvas
+      * @param {int} y The y coordinate where to place the image on the canvas
+      * @param {int} w The width of the image to use (stretch or reduce the image)
+      * @param {int} h The height of the image to use (stretch or reduce the image)
+      * @param {int} cX The x coordinate where to start clipping
+      * @param {int} cY The y coordinate where to start clipping
+      * @param {int} cW The width of the clipped image
+      * @param {int} cH The height of the clipped image
      */
 
-    // Display image size
-    img_dwidth = canvasScale * main_img.width;
-    img_dheight = canvasScale * main_img.height;
-    // Reload image ? 
-    main_img = new Image();
-    // Clear context
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height, 0, 0, ctx.canvas.width, ctx.canvas.height);
-    // Load Image    
-    main_img.onload = function () {
-        // Draw image
-        ctx.drawImage(main_img, canvas_cx, canvas_cy, img_width, img_height, canvas_dx, canvas_dy, img_dwidth, img_dheight);
-    }
-    main_img.src = currentSrc;
+    // posição inicial x
+    if (x != null && x != undefined)
+        this.canvasX = x;
+    else
+        this.canvasX = 0;
+    // posição inicial y
+    if (y != null && y != undefined)
+        this.canvasY = y;
+    else
+        this.canvasY = 0;
+    // altura canvas - h
+    if (h != null && h != undefined)
+        this.canvasH = h;
+    else
+        this.canvasH = 100;
+    // largura canvas - w
+    if (w != null && w != undefined)
+        this.canvasW = w;
+    else
+        this.canvasW = 100;
+    // inicio corte - x
+    if (cX != null && cX != undefined)
+        this.cropX = cX;
+    else
+        this.cropX = 0;
+    // inicio corte - y
+    if (cY != null && cY != undefined)
+        this.cropY = cY;
+    else
+        this.cropY = 0;
+    // altura corte
+    if (cH != null && cH != undefined)
+        this.cropH = cH;
+    else
+        this.cropH = 100;
+    // largura corte
+    if (cW != null && cW != undefined)
+        this.cropW = cW;
+    else
+        this.cropW = 100;
 }
+
+function canvasZoom(clicks, mouseX, mouseY) {
+    /** @description Set canvas zoom factor
+      * @param {float} clicks increase of zoom (positive or negative)
+      * @param {int} mouseX mouse x coord over canvas
+      * @param {int} mouseY mouse y coord over canvas
+     */
+    // Factor for zoom
+    var scaleFactor = 1.1;
+    // Current transformations applied to context
+    var c_status = ctx.getTransform();
+    // Zoom factor
+    var factor = Math.pow(scaleFactor, clicks);
+    console.log(factor);
+    // New zoom transformation factor
+    var tfactor = c_status.a * factor;
+    // Apply conditions
+    if (tfactor < 1) {
+        refreshCanvas();// redraw(true);
+    }
+    else if (tfactor < 10 * 1 / canvasScale) {
+        // Current location
+        var pt = ctx.transformedPoint(lastX, lastY);
+        // Translate
+        ctx.translate(pt.x, pt.y);
+        // Scale
+        ctx.scale(factor, factor);
+        // Translate back with new coord
+        ctx.translate(-pt.x, -pt.y);
+        // Redraw image
+        refreshCanvas();//redraw(false);
+    }
+}
+
+
+/*
+ * Event Handler
+ */
 
 // Canvas controls
 var lastX, lastY;
 var dragStart = null;
 var dragging = false;
+
+function canvasMouseUp(evt) {
+    /** @description Canvas Mouse Up event
+      * @param {event} evt event
+     */
+
+    // Current transformations applied to context
+    if (dragging) {
+        var c_status = ctx.getTransform();
+        if (c_status.a > 1) {
+            //  dragStart = null;
+            dragging = false;
+        }
+    }
+}
+
+function canvasScrollWheel(evt) {
+    /** @description Canvas Mouse Scroll Wheel event
+      * @param {event} ev Scroll Wheel event
+     */
+    lastX = evt.offsetX;
+    lastY = evt.offsetY;
+    var delta = evt.wheelDelta ? evt.wheelDelta / 40 : evt.detail ? -evt.detail : 0;
+    if (delta) canvasZoom(delta, lastX, lastY);
+    return evt.preventDefault() && false;
+}
 
 function canvasMouseDown(evt) {
     /** @description Canvas Mouse Down event
@@ -207,90 +298,10 @@ function canvasMouseMove(evt) {
             if (((!moveUp) && (dy < 0)) || ((!moveDown) && (dy > 0))) { dy = 0; }
             // Move image
             ctx.translate(dx, dy);
-            redraw(false);
+            refreshCanvas();//redraw(false);
         }
     }
 }
-
-function canvasMouseUp(evt) {
-    /** @description Canvas Mouse Up event
-      * @param {event} evt event
-     */
-
-    // Current transformations applied to context
-    if (dragging) {
-        var c_status = ctx.getTransform();
-        if (c_status.a > 1) {
-            //  dragStart = null;
-            dragging = false;
-        }
-    }
-}
-
-function canvasScrollWheel(evt) {
-    /** @description Canvas Mouse Scroll Wheel event
-      * @param {event} ev Scroll Wheel event
-     */
-    lastX = evt.offsetX;
-    lastY = evt.offsetY;
-    var delta = evt.wheelDelta ? evt.wheelDelta / 40 : evt.detail ? -evt.detail : 0;
-    if (delta) canvasZoom(delta, lastX, lastY);
-    return evt.preventDefault() && false;
-}
-
-function canvasZoom(clicks, mouseX, mouseY) {
-    /** @description Set canvas zoom factor
-      * @param {float} clicks increase of zoom (positive or negative)
-      * @param {int} mouseX mouse x coord over canvas
-      * @param {int} mouseY mouse y coord over canvas
-     */
-    // Factor for zoom
-    var scaleFactor = 1.1;
-    // Current transformations applied to context
-    var c_status = ctx.getTransform();
-    // Zoom factor
-    var factor = Math.pow(scaleFactor, clicks);
-    // New zoom transformation factor
-    var tfactor = c_status.a * factor;
-    // Apply conditions
-    if (tfactor < 1) {
-        redraw(true);
-    }
-    else if (tfactor < 10 * 1 / canvasScale) {
-        // Current location
-        var pt = ctx.transformedPoint(lastX, lastY);
-        // Translate
-        ctx.translate(pt.x, pt.y);
-        // Scale
-        ctx.scale(factor, factor);
-        // Translate back with new coord
-        ctx.translate(-pt.x, -pt.y);
-        // Redraw image
-        redraw(false);
-    }
-}
-
-function redraw(reset) {
-    /** @description Redraw canvas
-      * @param {bool} reset if true, reset canvas to original size
-     */
-    if (reset) {
-        // Clear transformations
-        ctx.setTransform(1, 0, 0, 1, 0, 0);
-        // Clear canvas
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
-    else {
-        // Get new coord
-        var p1 = ctx.transformedPoint(0, 0);
-        var p2 = ctx.transformedPoint(canvas.width, canvas.height);
-        // Crop canvas
-        ctx.clearRect(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y);
-    }
-    // Draw Image
-    ctx.drawImage(main_img, canvas_cx, canvas_cy, main_img.width, main_img.height, canvas_dx, canvas_dy, img_dwidth, img_dheight);
-}
-
 
 
 /*
@@ -354,62 +365,4 @@ function trackTransforms(ctx) {
         pt.x = x; pt.y = y;
         return pt.matrixTransform(xform.inverse());
     }
-}
-
-
-/*
- * * Canvas Size
- */
- 
-function CanvasSizes(x, y, w, h, cX, cY, cW, cH) {
-    /// <summary>Classe para guardar posições do canvas</summary>
-    /// <param name="x" type="Object"></param>
-    /// <param name="y" type="Object"></param>
-    /// <param name="w" type="Object"></param>
-    /// <param name="h" type="Object"></param>
-    /// <param name="cX" type="Object"></param>
-    /// <param name="cY" type="Object"></param>
-    /// <param name="cW" type="Object"></param>
-    /// <param name="cH" type="Object"></param>
-    
-    // posição inicial x
-    if (x != null && x != undefined)
-        this.canvasX = x;
-    else
-        this.canvasX = 0;
-    // posição inicial y
-    if (y != null && y != undefined)
-        this.canvasY = y;
-    else
-        this.canvasY = 0;
-    // altura canvas - h
-    if (h != null && h != undefined)
-        this.canvasH = h;
-    else
-        this.canvasH = 100;
-    // largura canvas - w
-    if (w != null && w != undefined)
-        this.canvasW = w;
-    else
-        this.canvasW = 100;
-    // inicio corte - x
-    if (cX != null && cX != undefined)
-        this.cropX = cX;
-    else
-        this.cropX = 0;
-    // inicio corte - y
-    if (cY != null && cY != undefined)
-        this.cropY = cY;
-    else
-        this.cropY = 0;
-    // altura corte
-    if (cH != null && cH != undefined)
-        this.cropH = cH;
-    else
-        this.cropH = 100;
-    // largura corte
-    if (cW != null && cW != undefined)
-        this.cropW = cW;
-    else
-        this.cropW = 100;
 }
