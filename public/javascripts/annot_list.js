fe62080d480df2fe931bc25993b918f4a7dc122a
ARGONATH\Diego <dswanderley@gmail.com>
2018-08-31 14:05:52 +0100

    CHANGE: Adjust right col and and respective tables

--- public/javascripts/annot_list.js
+++ public/javascripts/annot_list.js
@@ -17,17 +17,17 @@ function listTable(body, el_str, el_list) {
         // Table of content
         var table = document.createElement("table");
         table.setAttribute("id", "table-" + el_str.toLowerCase());
-        table.setAttribute("class", "table-dark");
+        table.setAttribute("class", "table table-striped");
         // Header elements
         var header = document.createElement("tr");
         var th1 = document.createElement("th");
         th1.appendChild(document.createTextNode(el_str));
         header.appendChild(th1);
         var th2 = document.createElement("th");
-        th2.appendChild(document.createTextNode("Major axis"));
+        th2.appendChild(document.createTextNode("Major"));
         header.appendChild(th2);
         var th3 = document.createElement("th");
-        th3.appendChild(document.createTextNode("Minor axis"));
+        th3.appendChild(document.createTextNode("Minor"));
         header.appendChild(th3);
         var th4 = document.createElement("th");
         th4.appendChild(document.createTextNode("Edit"));
@@ -35,7 +35,12 @@ function listTable(body, el_str, el_list) {
         var th5 = document.createElement("th");
         th5.appendChild(document.createTextNode("Delete"));
         header.appendChild(th5);
-        table.appendChild(header);
+        // Append header
+        var thead = document.createElement("thead");
+        thead.appendChild(header);
+        table.appendChild(thead);
+        // Start body
+        var tbody = document.createElement("tbody");
         // Read all list of elements
         for (i = 0; i < ovary_list.length; i++) {
             // Element id to be listed
@@ -47,7 +52,7 @@ function listTable(body, el_str, el_list) {
             // Text Elements
             var td1 = document.createElement("td");
             td1.setAttribute("class", "td-text-annot");
-            td1.appendChild(document.createTextNode(el[0] + " #" + idx));
+            td1.appendChild(document.createTextNode("#" + idx));
             row.appendChild(td1);
             var td2 = document.createElement("td");
             td2.setAttribute("class", "td-text-annot");
@@ -58,22 +63,23 @@ function listTable(body, el_str, el_list) {
             td3.appendChild(document.createTextNode(el[2] + "px"));
             row.appendChild(td3);
             // Edit Button
-            var td4 = document.createElement("td"); td4.setAttribute("class", "td-btn-annot");
-            td4.appendChild(document.createTextNode("100 px"));
-            td4.innerHTML = '<button type="button" class="btn btn-light btn-list">' +
-                '<i class="far fa-edit" ></i >' + '</button>'
+            var td4 = document.createElement("td");
+            td4.setAttribute("class", "td-btn-annot");
+            td4.innerHTML = '<button type="button" class="btn btn-light btn-annot">' +
+                '<i class="far fa-edit i-tab" ></i >' + '</button>'
             row.appendChild(td4);
             // Delete Button 
-            var td5 = document.createElement("td"); td5.setAttribute("class", "td-btn-annot");
-            td5.appendChild(document.createTextNode("100 px"));
-            td5.innerHTML = '<button type="button" class="btn btn-primary btn-list">' +
-                '<i class="far fa-trash-alt" ></i >' + '</button>'
+            var td5 = document.createElement("td");
+            td5.setAttribute("class", "td-btn-annot");
+            td5.innerHTML = '<button type="button" class="btn btn-primary btn-annot">' +
+                '<i class="far fa-trash-alt i-tab" ></i >' + '</button>'
             row.appendChild(td5);
 
             // Append Row to table
-            table.appendChild(row);
+            tbody.appendChild(row)
         }
         // Append table to body
+        table.appendChild(tbody);
         body.appendChild(table);
     }    
 }
