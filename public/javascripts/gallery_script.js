f65afc0be596d734d80cd9b8f6073a84ecd4da47
ARGONATH\Diego <dswanderley@gmail.com>
2019-04-16 18:09:20 +0100

    ADD: Date Ranger Picker.

--- public/javascripts/gallery_script.js
+++ public/javascripts/gallery_script.js
@@ -16,4 +16,16 @@ var search_status = 'all';
 
 function loadGallery() {
     $('[data-toggle="tooltip"]').tooltip();
+    dateRangePicker();
+}
+
+
+function dateRangePicker() {
+
+    $('input[name="daterange"]').daterangepicker({
+        opens: 'right'
+      }, function(start, end, label) {
+        console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+      });
+
 }
