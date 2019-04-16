fe1945b3ff58b7a9e05f6b1a33822505ce8e2fd8
ARGONATH\Diego <dswanderley@gmail.com>
2019-04-16 18:46:21 +0100

    CHANGE: Edit dateRangePicker to allow empty input.

--- public/javascripts/gallery_script.js
+++ public/javascripts/gallery_script.js
@@ -15,17 +15,40 @@ var search_type = 'all';
 var search_status = 'all';
 
 function loadGallery() {
+    /** @description starts the gallery of images.
+     */
+    // Turn on tooltips bootstrap
     $('[data-toggle="tooltip"]').tooltip();
+    // Turn on date ranger
     dateRangePicker();
 }
 
 
 function dateRangePicker() {
-
+    /** @description set functions and default values of datarange fields.
+     */
     $('input[name="daterange"]').daterangepicker({
-        opens: 'right'
+        autoUpdateInput: false,
+        locale: {
+            cancelLabel: 'Clear'
+        },
+        buttonClasses: "btn btn-sm",
+        applyButtonClasses: "btn-info",
+        cancelClass: "btn-default",
+        drops: 'up',
+        opens: 'right',
+        minYer: 2017,
+        maxYear: 2019
       }, function(start, end, label) {
         console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
       });
+
+    $('input[name="daterange"]').on('apply.daterangepicker', function(ev, picker) {
+        $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
+    });
+  
+    $('input[name="daterange"]').on('cancel.daterangepicker', function(ev, picker) {
+        $(this).val('');
+    });
 
 }
